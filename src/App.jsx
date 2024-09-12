e11b721d51da43edc56a4a72038431bc85965730
alanVelazquez14 <velazquez.alan14@gmail.com>
2024-09-12 14:25:48 -0300

    layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
 import { Toaster } from "sonner";
 import { Footer } from "./components/Footer/Footer";
-// import { Layout } from "./components/Layout/Layout";
+import { Layout } from "./components/Layout/Layout";
 import Routes from "./routes/Routes";
 
 function App() {
   return (
-    // <Layout>
-    <>
+    <Layout>
       <Toaster richColors closeButton />
       <Routes />
       <Footer />
-    </>
-    // </Layout>
+    </Layout>
   );
 }
 
